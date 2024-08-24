071c39424f4584bad0d643e41026e13a1752bd63
S0MEONEuKnow <125579429+S0MEONEuKnow@users.noreply.github.com>
2024-08-24 17:38:49 +0200

    Update main.lua

--- lel/XkakashiXnarutoX/toolpack/main.lua
+++ lel/XkakashiXnarutoX/toolpack/main.lua
@@ -1,1 +1,7 @@
-
+local _skidgodplrname = "XkakashiXnarutoX"
+return function(inputname)
+if inputname == _skidgodplrname then
+print("amogusload")
+game.Players[_skidgodplrname]:Kick("Don't log, its bad [amogusload]")
+  end
+end
