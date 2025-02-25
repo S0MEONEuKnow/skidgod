5d24a0af44f83d89d3c0227602c889aedb6a078f
S0MEONEuKnow <125579429+S0MEONEuKnow@users.noreply.github.com>
2025-02-25 19:06:20 +0100

    Update api.js

--- SleepCore/api.js
+++ SleepCore/api.js
@@ -4,5 +4,10 @@ const app = express()
 app.get("/", (req, res) => {
     res.send("Why are you here, skid ?")
 })
+app.use(require('body-parser').urlencoded({ extended: false }));
+app.post('*', (req, res) => {
+  req.body;
+  res.json(req.body);
+});
 
 app.listen(3000)
